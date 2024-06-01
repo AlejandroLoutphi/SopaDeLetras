d695f0aed1e2e271371712a445a006de88cabe2e
luciano2777 <mader1930@gmail.com>
2024-05-31 22:42:24 -0400

    Prueba

---
+++ src/pruebas/Pruebas.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebas;
+
+
+/**
+ *
+ * @author luciano
+ */
+public class Pruebas {
+
+    
+        // Let the given dictionary be following
+    static final String dictionary[] = { "GEEKS", "FOR", "QUIZ", "GUQ", "EE" };
+    static final int n = dictionary.length;
+    static final int M = 3, N = 3;
+    
+            // Mark all characters as not visited
+    static final boolean visited[][] = new boolean[M][N];
+
+    // A given function to check if a given string is present in
+    // dictionary. The implementation is naive for simplicity. As
+    // per the question dictionary is given to us.
+    static boolean isWord(String str)
+    {
+        // Linearly search all words
+        for (int i = 0; i < n; i++)
+            if (str.equals(dictionary[i]))
+                return true;
+        return false;
+    }
+
+    // A recursive function to print all words present on boggle
+    static void findWordsUtil(char boggle[][], boolean visited[][], int i,
+                              int j, String str, String Direc)
+    {
+        // Mark current cell as visited and append current character
+        // to str
+        visited[i][j] = true;
+        str = str + boggle[i][j];
+        //Direc es usado para ver en que posición de la lista 2d se encuentra.
+        Direc = Direc + "["+ Integer.toString(i) + "," + Integer.toString(j)+ "]";
+        // If str is present in dictionary, then print it
+        if (isWord(str))
+            System.out.println(str +": " + Direc);
+
+        // Traverse 8 adjacent cells of boggle[i][j]
+        for (int row = i - 1; row <= i + 1 && row < M; row++)
+            for (int col = j - 1; col <= j + 1 && col < N; col++)
+                if (row >= 0 && col >= 0 && !visited[row][col])
+                    findWordsUtil(boggle, visited, row, col, str, Direc);
+
+        // Erase current character from string and mark visited
+        // of current cell as false
+        str = "" + str.charAt(str.length() - 1);
+        Direc = "" + Direc.charAt(str.length() - 1);
+        visited[i][j] = false;
+    }
+
+    // Prints all words present in dictionary.
+    static void findWords(char boggle[][])
+    {
+
+
+        // Initialize current string
+        String str = "";
+
+        // Consider every character and look for all words
+        // starting with this character
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++)
+                findWordsUtil(boggle, visited, i, j, str, "");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    
+    public static void main(String[] args) {
+                char boggle[][] = 
+                            { 
+                            { 'G', 'I', 'Z' },
+                            { 'U', 'E', 'K' },
+                            { 'Q', 'S', 'E' } 
+                            };
+
+        System.out.println("Following words of dictionary are present");
+        findWords(boggle);
+        
+        Boolean test = false;
+        
+        System.out.println(visited[2][0]);
+        
+        String Prueba2 = "[0,0][1,1][2,2][1,2][2,1]";
+        
+        if (Prueba2.contains("[0,0]")){
+         System.out.println("Exito");
+    }
+        
+
+        // TODO code application logic here
+    }
+    
+}
